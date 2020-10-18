9edd2ed989cee1b44532a95e6060547e31a9d9a1
beep-boop-stof <loveleenbassi761@gmail.com>
2020-10-17 20:05:43 -0700

    removed the numbers from input

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ void influencers();
 void q_and_a(vector<string>, vector<string>);
 void selection();
 void play();
+bool checkString(string);
 
 int main(int argc, char **argv)
 {
@@ -105,7 +106,7 @@ void q_and_a(vector<string> q, vector<string> a) {
         //char response[100];
         string response;
         //cin.ignore();
-        getline(cin, response);
+        if(i==0) getline(cin, response);
         getline(cin, response);
         //cin.getline(response, 100);
 
@@ -144,47 +145,48 @@ void selection(){
     char input;
     while (x == 0)
     {
-        cout << "         (1) Vocabulary" << endl;
-        cout << "         (2) History" << endl;
-        cout << "         (3) Influencers" << endl;
-        cout << "         (4) Blah" << endl;
-        cout << "         (5) BlahBlah\n"<< endl;
-
+        cout << "          * Vocabulary" << endl;
+        cout << "          * History" << endl;
+        cout << "          * Influencers" << endl;
+        //cout << "         (4) Blah" << endl;
+        //cout << "         (5) BlahBlah\n"<< endl;
+        
+        
         cin >> option;
         input = option.at(0);
         cout << "You picked " << option << "!" << endl;
 
         switch (input)
         {
-        case 'V':
-        case 'v':
-            x = 1;
-            //cout<<"Case1"<<endl;
-            vocabulary();
-            break;
-        case 'H':
-        case 'h':
-            x = 1;
-            //cout<<"Case2"<<endl;
-            history();
-            break;
-        case 'I':
-        case 'i':
-            x = 1;
-            //cout<<"Case3"<<endl;
-            influencers();
-            break;
-        default:
-            cout << "Invalid Option! Try Again\n"
-                 << endl;
+            case 'V':
+            case 'v':
+               x = 1;
+                //cout<<"Case1"<<endl;
+                vocabulary();
+                break;
+            case 'H':
+            case 'h':
+                x = 1;
+                //cout<<"Case2"<<endl;
+                history();
+                break;
+            case 'I':
+            case 'i':
+                x = 1;
+                //cout<<"Case3"<<endl;
+                influencers();
+                break;
+            default:
+                cout << "Invalid Option! Try Again\n" << endl;
         }
+        
     }
 
 }
 
 void play(){
     //int points;
-    int result;
+    //int result;
     int x=0;
     string playAgain;
     char again;
@@ -210,5 +212,3 @@ void play(){
         }
     }
 }
-
-
