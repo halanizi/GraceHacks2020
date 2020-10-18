c5535cd29b6874c65e9a3651be2417d2c845a03b
beep-boop-stof <loveleenbassi761@gmail.com>
2020-10-17 17:08:01 -0700

    small changes

--- main.cpp
+++ main.cpp
@@ -15,8 +15,6 @@ void play();
 
 int main(int argc, char **argv)
 {
-    int points;
-    int result;
     cout << "Welcome! Pick a category to start the game:\n"<< endl;
     selection();
     play();
@@ -173,28 +171,30 @@ void selection(){
 }
 
 void play(){
-int x=0;
-string playAgain;
+    int points;
+    int result;
+    int x=0;
+    string playAgain;
     char again;
     while(x==0){
-    cout<<"Would you like to choose another category?"<<endl;
-    cin>>playAgain;
-    again = playAgain.at(0);
-    switch(again){
-        case 'Y':
-        case 'y':
-            //start the main again
-            cout<<"Thank you for playing again! Pick another category!"<<endl;
+        cout<<"Would you like to choose another category?"<<endl;
+        cin>>playAgain;
+        again = playAgain.at(0);
+        switch(again){
+            case 'Y':
+            case 'y':
+                //start the main again
+                cout<<"Thank you for playing again! Pick another category!"<<endl;
 
-            selection();
-            break;
+                selection();
+                break;
         
-        case 'N':
-        case 'n':
-        x=1;
-            cout<<"Thanks for playing!"<<endl;
-            break;
-    }
+            case 'N':
+            case 'n':
+                x=1;
+               cout<<"Thanks for playing!"<<endl;
+                break;
+        }
     }
 }
 
